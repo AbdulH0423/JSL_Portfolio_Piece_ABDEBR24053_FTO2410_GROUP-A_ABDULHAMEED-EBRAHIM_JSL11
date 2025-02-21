16104d36c5f42f1f3bdfa8c1253bdb01c1781bd2
Abdul-Hameed Zainud-Deen Ebrahim <zain.ebrahim8@gmail.com>
2025-02-21 16:18:38 +0200

    added logic to toggle theme
    added logic to toggle theme

--- index.js
+++ index.js
@@ -286,7 +286,8 @@ function toggleSidebar(show) {
 }
 
 function toggleTheme() {
- 
+  const isLightTheme = document.body.classList.toggle('light-theme');
+  localStorage.setItem('light-theme', isLightTheme ? 'enabled' : 'disabled');
 }
 
 
